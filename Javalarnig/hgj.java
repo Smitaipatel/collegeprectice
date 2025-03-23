8162178e911e5267e42c36103733df36ed5bd8f9
Smit Patel <smitnpatel55@gmail.com>
2025-03-23 20:01:08 +0530

    the

---
+++ Javalarnig/hgj.java
@@ -0,0 +1,18 @@
+public class hgj {
+    public static void main(String[] args) {
+        int x = 19;
+        int y = 9;
+        //x=x + y+ 5*2;
+        System.out.println(x >= y);
+        if(x >= y || x >10)
+            {System.out.println( x + y);
+            System.out.println("my name is smit");}
+
+        // }
+         else
+            System.out.println("y is biger than x");
+        
+        System.out.println(x);
+        System.out.println(y);
+  }
+}
---
+++ Javalarnig/klj.java
@@ -0,0 +1,6 @@
+public class klj  {
+    public static void main(String[] args){
+        int x =30;
+        System.out.println(x);
+    }
+}
