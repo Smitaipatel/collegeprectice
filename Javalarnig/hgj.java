87f08cc09241e35382eb1de1f857faf5bff27103
Smit Patel <smitnpatel55@gmail.com>
2025-03-24 16:49:20 +0530

    the

--- Javalarnig/hgj.java
+++ Javalarnig/hgj.java
@@ -4,6 +4,7 @@ public class hgj {
         int y = 9;
         //x=x + y+ 5*2;
         System.out.println(x >= y);
+        System.out.println(x <= y);
         if(x >= y || x >10)
             {System.out.println( x + y);
             System.out.println("my name is smit");}
@@ -14,5 +15,14 @@ public class hgj {
         
         System.out.println(x);
         System.out.println(y);
+
+        System.out.println(++x);//Increment
+        System.out.println(x % y);//Modulus
+        System.out.println(--x);//Decrement
+        System.out.println(x %= 3);//Assignment
+        System.out.println( x &= 3);//Assignment
+        System.out.println(x ^= 3);//Assignment
+        System.out.println(x != y);//Not equal
+        System.out.println(!(x > 3 && x < 10));//Logical not
   }
 }
