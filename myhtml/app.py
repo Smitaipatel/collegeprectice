a9d2ece56f04bb971099ad3a0be1a2294c8cc08b
Smit Patel <smitnpatel55@gmail.com>
2025-07-30 16:27:37 +0530

    adf

--- myhtml/app.py
+++ myhtml/app.py
@@ -1,15 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template_string
 import pyodbc
 
 app = Flask(__name__)
 
-# SQL Server connection string
 conn_str = (
     'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=DESKTOP-65JLCDI\SQLEXPRESS;'
-    'DATABASE=SmitPractical;'
+    'SERVER=localhost\\SQLEXPRESS;'
+    'DATABASE=SidcupGolf;'
     'Trusted_Connection=yes;'
 )
+# HTML template with form
+form_html = '''
+<!DOCTYPE html>
+<html>
+<head><title>Contact</title></head>
+<body>
+    <h2>Contact Form</h2>
+    <form method="POST">
+        <label>Name:</label><br>
+        <input type="text" name="name" required><br>
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br>
+        <label>Message:</label><br>
+        <textarea name="message" rows="4" required></textarea><br><br>
+        <button type="submit">Submit</button>
+    </form>
+    <p>{{ msg }}</p>
+</body>
+</html>
+'''
 
 @app.route('/', methods=['GET', 'POST'])
 def contact():
@@ -32,7 +51,7 @@ def contact():
         except Exception as e:
             msg = f"Error: {e}"
 
-    return render_template("contact.html", msg=msg)
+    return render_template_string(form_html, msg=msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
